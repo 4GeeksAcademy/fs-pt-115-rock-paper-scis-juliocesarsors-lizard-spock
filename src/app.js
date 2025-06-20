378fb3ab23bdc4993137d64b3d8523a4825934d0
24399598 <juliowork.1309@gmail.com>
2025-06-20 10:23:34 +0000

    mi proyecto

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
 //write your code here
 console.log("Hello Rigo from the console!");
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
+const rules = {
+  rock:     ["scissors", "lizard"],
+  paper:    ["rock", "spock"],
+  scissors: ["paper", "lizard"],
+  lizard:   ["spock", "paper"],
+  spock:    ["scissors", "rock"]
+};
+
+function getWinner(player, cpu) {
+  if (player === cpu) {
+    return "Empate";
+  } else if (rules[player].includes(cpu)) {
+    return "Ganaste";
+  } else {
+    return "Perdiste";
+  }
+}
+
+
+const playerChoice = "spock";
+const cpuChoice = choices[Math.floor(Math.random() * choices.length)];
+
+console.log(`Tú elegiste: ${playerChoice}`);
+console.log(`La computadora eligió: ${cpuChoice}`);
+console.log(getWinner(playerChoice, cpuChoice));
